2af7c70b882fcf8653d44716b0c0c2b3c28b4f7c
TrieuVanBan <79831622+TrieuVanBan@users.noreply.github.com>
2023-04-07 00:43:18 +0700

    abc

--- index.js
+++ index.js
@@ -1,59 +1,66 @@
-let str = ' "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BCz" '
-
+let str =
+  ' "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BCz" ';
 
 // câu 1: Lấy các chữ cái, bỏ chữ số và khoảng trống
-let letterStr = str.match(/[a-z]/gi).join('');
+let letterStr = str.match(/[a-z]/gi).join("");
 console.log("Câu 1:", letterStr);
-document.write("Câu 1:", letterStr)
-
-// 101 / 4 = 25
-// 101 % 4 = 1
-
-
+document.write("Câu 1:", letterStr);
 
 // câu 2: Chia mảng cha thành 4 mảng nhỏ
 let charArr = letterStr.split("");
-// const surplus = charArr % 4
-let arr = charArr.length / 4
+let arr = charArr.length / 4;
 
 function sumArray(myArray, sum) {
-    var results = [];
+  var index = 0;
+  var tempArray = [];
 
-    while (myArray.length) {
-        results.push(myArray.splice(0, sum));
-    }
-
-    return results;
+  for (index = 0; index < myArray.length; index += sum) {
+    myChunk = myArray.slice(index, index + sum);
+    // Do something if you want with the group
+    tempArray.push(myChunk);
+  }
+  return tempArray;
 }
 let result = sumArray(charArr, arr);
 console.log("Câu 2:", result);
 
 // Câu 3: sắp xếp các mảng từ bé đến lớn
+result.map((item) => {
+  //   console.log("item :", item.sort());
+  const sortArr = item.sort(function (a, b) {
+    let left = a.toUpperCase();
+    let right = b.toUpperCase();
 
-result.sort(function(a, b) {
-    let x = a.toLowerCase();
-    let y = b.toLowerCase();
-
-    return x == y ? 0 : (x > y) ? 1 : -1;
+    return left === right ? 0 : left > right ? 1 : -1;
+  });
+  console.log("Câu 3:", sortArr);
 });
-console.log(result);
 
 // câu 4: Thêm dấu * vào từng phần tử con
 const itemsArr = result.map((items) => {
-
-    const itemArr = items.map((element) => {
-        let arrElement = element + "*"
-        return arrElement
-    });
-    return itemArr
-})
+  const itemArr = items.map((element) => {
+    let arrElement = element + "*";
+    return arrElement;
+  });
+  return itemArr;
+});
 console.log("Câu 4:", itemsArr);
 
-
-
-
 // Câu 5: Tìm các item có chứa chữ 'e|E' lưu ra mảng
-
-
+const arrItems = itemsArr.map((items) => {
+  items.filter((item) => console.log(item));
+  // const array = items.map((element) => {
+  //   let arrElement = element.filter(e => console.log(e))
+  //   console.log(arrElement);
+  // });
+});
 
 // Câu 6: Đổi '*' thành ','
+const arrItems6 = itemsArr.map((items) => {
+  const arr = items.map((item) => {
+    const replaceItem = item.replace("*", ",");
+    return replaceItem;
+  });
+  return arr.join("");
+});
+console.log(arrItems6.join(""));
